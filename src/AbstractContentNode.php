af34e54c60141ab22137bbefc9a1f33a69487b50
Carsten Bleicker <carsten@bleicker.de>
2015-04-28 19:51:25 +0200

    Introduce AbstractContentNode::$children

--- src/AbstractContentNode.php
+++ src/AbstractContentNode.php
@@ -2,6 +2,9 @@
 
 namespace Bleicker\Nodes;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
 /**
  * Class AbstractContentNode
  *
@@ -21,4 +24,58 @@ class AbstractContentNode implements NodeInterface {
 		return $this->id;
 	}
 
+	/**
+	 * @var integer
+	 */
+	protected $sorting;
+
+	/**
+	 * @var AbstractPageNode
+	 */
+	protected $parent;
+
+	/**
+	 * @var Collection
+	 */
+	protected $children;
+
+	/**
+	 * @param string $title
+	 */
+	public function __construct($title) {
+		$this->title = $title;
+		$this->children = new ArrayCollection();
+	}
+
+	/**
+	 * @param integer $sorting
+	 * @return $this
+	 */
+	public function setSorting($sorting = NULL) {
+		$this->sorting = $sorting;
+		return $this;
+	}
+
+	/**
+	 * @return integer
+	 */
+	public function getSorting() {
+		return $this->sorting;
+	}
+
+	/**
+	 * @param \Bleicker\Nodes\AbstractPageNode $parent
+	 * @return $this
+	 */
+	public function setParent($parent = NULL) {
+		$this->parent = $parent;
+		return $this;
+	}
+
+	/**
+	 * @return \Bleicker\Nodes\AbstractPageNode
+	 */
+	public function getParent() {
+		return $this->parent;
+	}
 }
