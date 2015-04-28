95eebb646dfb81ed4349b748d2250d22e5b805be
Carsten Bleicker <carsten@bleicker.de>
2015-04-28 20:38:46 +0200

    ContentNodeService supports various methods

--- src/Service/ContentNodeService.php
+++ src/Service/ContentNodeService.php
@@ -3,6 +3,7 @@
 namespace Bleicker\Nodes\Service;
 
 use Bleicker\Nodes\AbstractContentNode;
+use Bleicker\Nodes\ContentNodeInterface as NodeInterface;
 
 /**
  * Class ContentNodeService
@@ -16,5 +17,89 @@ class ContentNodeService extends AbstractNodeService {
 	 */
 	public function getType() {
 		return AbstractContentNode::class;
+	}
+
+	/**
+	 * @param NodeInterface $node
+	 * @param NodeInterface $into
+	 * @return $this
+	 */
+	public function into(NodeInterface $node, NodeInterface $into) {
+		/** @var NodeInterface $lastChild */
+		$lastChild = $into->getChildren()->last();
+		$node->setParent($into)->setSorting($lastChild === FALSE ? 0 : $lastChild->getSorting() + NodeInterface::SORTING_DIFF);
+		$into->getChildren()->add($node);
+		$this->entityManager->persist($into);
+		return $this;
+	}
+
+	/**
+	 * @param NodeInterface $node
+	 * @param NodeInterface $after
+	 * @return $this
+	 */
+	public function after(NodeInterface $node, NodeInterface $after) {
+		$node->setSorting($after->getSorting() + 1);
+		$node->setParent($after->getParent());
+		$node->getParent()->getChildren()->add($node);
+
+		$children = [];
+
+		/** @var NodeInterface $child */
+		foreach ($node->getParent()->getChildren() as $child) {
+			$children[$child->getSorting()] = $child;
+		}
+
+		ksort($children);
+
+		$index = 0;
+
+		/** @var NodeInterface $child */
+		foreach ($children as $child) {
+			$node->getParent()->getChildren()->removeElement($child);
+			$child->setSorting($index * NodeInterface::SORTING_DIFF);
+			$node->getParent()->getChildren()->add($child);
+			$index++;
+		}
+
+		unset($children);
+
+		$this->entityManager->persist($node->getParent());
+		return $this;
+	}
+
+	/**
+	 * @param NodeInterface $node
+	 * @param NodeInterface $after
+	 * @return $this
+	 */
+	public function before(NodeInterface $node, NodeInterface $after) {
+		$node->setSorting($after->getSorting() - 1);
+		$node->setParent($after->getParent());
+		$node->getParent()->getChildren()->add($node);
+
+		$children = [];
+
+		/** @var NodeInterface $child */
+		foreach ($node->getParent()->getChildren() as $child) {
+			$children[$child->getSorting()] = $child;
+		}
+
+		ksort($children);
+
+		$index = 0;
+
+		/** @var NodeInterface $child */
+		foreach ($children as $child) {
+			$node->getParent()->getChildren()->removeElement($child);
+			$child->setSorting($index * NodeInterface::SORTING_DIFF);
+			$node->getParent()->getChildren()->add($child);
+			$index++;
+		}
+
+		unset($children);
+
+		$this->entityManager->persist($node->getParent());
+		return $this;
 	}
 }
