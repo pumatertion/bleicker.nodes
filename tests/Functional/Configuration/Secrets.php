c37f39225eb7761065e75f75674d9c4c72eadd76
Carsten Bleicker <carsten@bleicker.de>
2015-04-30 14:02:04 +0200

    Require Secrets.local.php if exits

--- tests/Functional/Configuration/Secrets.php
+++ tests/Functional/Configuration/Secrets.php
@@ -3,3 +3,7 @@
 use Bleicker\Registry\Registry;
 
 Registry::set('DbConnection', ['driver' => 'pdo_sqlite', 'path' => 'db.sqlite']);
+
+if(file_exists(__DIR__.'/Secrets.local.php')){
+	include __DIR__.'/Secrets.local.php';
+}
