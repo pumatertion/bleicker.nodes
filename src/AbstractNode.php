9dced16efd9e0eb40b5420a4ee48fb805127c625
Carsten Bleicker <carsten@bleicker.de>
2015-05-05 14:10:28 +0200

    Throw exceptions if add to itself as child

--- src/AbstractNode.php
+++ src/AbstractNode.php
@@ -2,6 +2,8 @@
 
 namespace Bleicker\Nodes;
 
+use Bleicker\Nodes\Exception\InvalidChildException;
+use Bleicker\Nodes\Exception\InvalidParentException;
 use Bleicker\Translation\TranslateTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -75,8 +77,12 @@ abstract class AbstractNode implements NodeInterface {
 	/**
 	 * @param NodeInterface $parent
 	 * @return $this
+	 * @throws InvalidParentException
 	 */
 	public function setParent(NodeInterface $parent = NULL) {
+		if($parent === $this){
+			throw new InvalidParentException('You can not move a node into itself', 1430826889);
+		}
 		if($parent === NULL && $this->getParent() !== NULL){
 			$this->getParent()->removeChild($this);
 		}
@@ -103,8 +109,12 @@ abstract class AbstractNode implements NodeInterface {
 	/**
 	 * @param NodeInterface $child
 	 * @return $this
+	 * @throws InvalidChildException
 	 */
 	public function addChild(NodeInterface $child) {
+		if($child === $this){
+			throw new InvalidChildException('You can not move a node into itself', 1430826890);
+		}
 		if($child->getParent() !== $this){
 			$child->setParent($this);
 		}
@@ -117,8 +127,12 @@ abstract class AbstractNode implements NodeInterface {
 	/**
 	 * @param NodeInterface $child
 	 * @return $this
+	 * @throws InvalidChildException
 	 */
 	public function removeChild(NodeInterface $child) {
+		if($child === $this){
+			throw new InvalidChildException('You can not remove a node from itself', 1430826891);
+		}
 		$child->setParent();
 		if($this->getChildren()->contains($child)){
 			$this->getChildren()->remove($child);
