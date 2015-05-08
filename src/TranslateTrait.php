eead3ba50b008db8d03e33e83908a5eafd6410e8
Carsten Bleicker <carsten@bleicker.de>
2015-05-08 13:43:32 +0200

    Add missing use statement

--- src/TranslateTrait.php
+++ src/TranslateTrait.php
@@ -3,6 +3,7 @@
 namespace Bleicker\Nodes;
 
 use Bleicker\Nodes\NodeTranslationInterface as TranslationInterface;
+use Bleicker\Translation\Exception\TranslationAlreadyExistsException;
 use Bleicker\Translation\TranslateTrait as TranslateTraitOrigin;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\Criteria;
